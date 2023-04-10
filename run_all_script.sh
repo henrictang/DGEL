efb833b06ca27edbc515b39440e050be47b3a284
henrictang <tang.henric@gmail.com>
2023-04-10 20:24:27 +0800

    Update the codes

--- run_all_script.sh
+++ run_all_script.sh
@@ -1,9 +1,5 @@
 CUDA_VISIBLE_DEVICES=0 python main.py --dataset wikipedia --embedding_dim 32 --sample_length 100 --epochs 30
 CUDA_VISIBLE_DEVICES=0 python evaluate_all.py --dataset wikipedia --embedding_dim 32 --sample_length 100 --epochs 30
 
-CUDA_VISIBLE_DEVICES=0 python main.py --dataset Foursquare --embedding_dim 32 --sample_length 200 --epochs 30
-CUDA_VISIBLE_DEVICES=0 python evaluate_all.py --dataset Foursquare --embedding_dim 32 --sample_length 200 --epochs 30
-
 CUDA_VISIBLE_DEVICES=0 python main.py --dataset reddit --embedding_dim 64 --sample_length 150 --epochs 30
 CUDA_VISIBLE_DEVICES=0 python evaluate_all.py --dataset redit --embedding_dim 64 --sample_length 150 --epochs 30
-
