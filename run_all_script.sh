5528de197a2ddeef7f093c9008052ab8b7e1e349
henrictang <tang.henric@gmail.com>
2024-11-18 18:59:15 +0800

    Update the codes

--- run_all_script.sh
+++ run_all_script.sh
@@ -2,4 +2,4 @@ CUDA_VISIBLE_DEVICES=0 python main.py --dataset wikipedia --embedding_dim 32 --s
 CUDA_VISIBLE_DEVICES=0 python evaluate_all.py --dataset wikipedia --embedding_dim 32 --sample_length 100 --epochs 30
 
 CUDA_VISIBLE_DEVICES=0 python main.py --dataset reddit --embedding_dim 64 --sample_length 150 --epochs 30
-CUDA_VISIBLE_DEVICES=0 python evaluate_all.py --dataset redit --embedding_dim 64 --sample_length 150 --epochs 30
+CUDA_VISIBLE_DEVICES=0 python evaluate_all.py --dataset reddit --embedding_dim 64 --sample_length 150 --epochs 30
